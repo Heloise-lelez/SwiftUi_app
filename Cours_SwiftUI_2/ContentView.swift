9ab6a4342134c06c58c1253d136cc802adc032ff
Héloïse Le Lez <lelez.heloise@gmail.com>
2024-10-15 12:12:29 +0200

    Initial Commit

---
+++ Cours_SwiftUI_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cours_SwiftUI_2
+//
+//  Created by Héloïse Le Lez on 15/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cours_SwiftUI_2/Cours_SwiftUI_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Cours_SwiftUI_2App.swift
+//  Cours_SwiftUI_2
+//
+//  Created by Héloïse Le Lez on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cours_SwiftUI_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
